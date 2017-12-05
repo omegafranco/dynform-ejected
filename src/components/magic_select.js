9c6d760f3ce24952de1e09945fc7430728e1d63e
Joao Franco <omegafranco@outlook.com>
2017-12-05 14:24:47 -0200

    projeto ejetado

--- src/components/magic_select.js
+++ src/components/magic_select.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
-// {Component} = const Component = React.Component
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 class MagicSelect extends Component {
     constructor(props) {
         super(props);
+        this.valueBuffer = [];
 
-        this.state = { value: props.value};
+        this.state = { 
+            value: props.value,
+            selectedOption: ''
+        };
     }
 
-    render() {        
-        return (
-            <div className={"form-group " + this.props.class} >
-                Futuro magic select
-            </div>
-        );      
+    handleChange = (selectedOptions) => {
+      this.setState({ selectedOptions });
+      this.setState({value: selectedOptions});
+      console.log(`Selected: ${ selectedOptions.map((option) => {return option.label}) }`);
     }
-}
+    
+    render() {
+      return (
+        <Select
+          name="form-field-name"
+          multi={true}
+          removeSelected={false}
+          value={this.state.value}
+          onChange={this.handleChange}
+          className={this.props.class}
+          options={[
+            { value: 'one', label: 'One' },
+            { value: 'two', label: 'Two' },
+            { value: 'three', label: 'Three' },
+          ]}
+        />
+      );
+    }
+  }
 
 export default MagicSelect;
